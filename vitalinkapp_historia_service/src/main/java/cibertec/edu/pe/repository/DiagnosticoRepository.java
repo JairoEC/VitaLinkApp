554e8ba086e27832198c135850e6a4bff1a68afb
yadi <yadi.tp01@gmail.com>
2026-06-05 05:29:52 -0500

    implementacion Diagnóstico (model, repo, service)

---
+++ vitalinkapp_historia_service/src/main/java/cibertec/edu/pe/repository/DiagnosticoRepository.java
@@ -0,0 +1,8 @@
+package cibertec.edu.pe.repository;
+
+import cibertec.edu.pe.model.Diagnostico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiagnosticoRepository extends JpaRepository<Diagnostico,Long> {
+
+}
---
+++ vitalinkapp_historia_service/src/main/java/cibertec/edu/pe/service/DiagnosticoService.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.service;
+
+import cibertec.edu.pe.model.Diagnostico;
+
+import java.util.List;
+
+public interface DiagnosticoService {
+    List<Diagnostico> listarTodos();
+    Diagnostico buscarPorId(Long id);
+    Diagnostico guardar(Diagnostico diagnostico);
+    Diagnostico actualizar(Long id, Diagnostico diagnosticoActualizado);
+    void eliminar(Long id);
+}
