9d52f2d963e25f82ba4394641e13fbb8e4244bc1
yadi <yadi.tp01@gmail.com>
2026-06-08 19:52:45 -0500

    nuevo Diagnostico

--- vitalinkapp_historia_service/src/main/java/cibertec/edu/pe/model/Diagnostico.java
+++ vitalinkapp_historia_service/src/main/java/cibertec/edu/pe/model/Diagnostico.java
@@ -1,4 +1,27 @@
 package cibertec.edu.pe.model;
 
+import cibertec.edu.pe.model.constantes.TipoDiagnostico;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="diagnosticos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Diagnostico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="consultaid")
+    private Long consultaId;
+    private String codigoCIE10;
+    private String descripcion;
+
+    @Enumerated(EnumType.STRING)
+    private TipoDiagnostico tipoDiagnostico;
 }
