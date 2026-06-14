026409ce6041f305e62635ce93b32c01dc0d74a5
Jennifer <jackcorrea49@gmail.com>
2026-06-14 17:40:17 -0500

    agregado repository DisponibilidadRepository, EspecialidadRepository, MedicoRepository y PacienteRepository

---
+++ vitalinkapp_citas_service/src/main/java/cibertec/edu/pe/repository/EspecialidadRepository.java
@@ -0,0 +1,7 @@
+package cibertec.edu.pe.repository;
+
+import cibertec.edu.pe.model.Especialidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EspecialidadRepository extends JpaRepository<Especialidad, Long> {
+}
