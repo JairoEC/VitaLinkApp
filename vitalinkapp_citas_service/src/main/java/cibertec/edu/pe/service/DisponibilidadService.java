cdf4c8a76029e72bb8b58a217ad0d5d815a5da3d
Jennifer <jackcorrea49@gmail.com>
2026-06-14 17:43:28 -0500

    agregado service DisponibilidadService, EspecialidadService, MedicoService y PacienteService

---
+++ vitalinkapp_citas_service/src/main/java/cibertec/edu/pe/service/DisponibilidadService.java
@@ -0,0 +1,42 @@
+package cibertec.edu.pe.service;
+
+import cibertec.edu.pe.model.Disponibilidad;
+import cibertec.edu.pe.repository.DisponibilidadRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DisponibilidadService {
+
+    private final DisponibilidadRepository disponibilidadRepository;
+
+    public List<Disponibilidad> listarTodas() {
+        return disponibilidadRepository.findAll();
+    }
+
+    public Optional<Disponibilidad> buscarPorId(Long id) {
+        return disponibilidadRepository.findById(id);
+    }
+
+    public Disponibilidad guardar(Disponibilidad disponibilidad) {
+        return disponibilidadRepository.save(disponibilidad);
+    }
+
+    public Disponibilidad actualizar(Long id, Disponibilidad disponibilidadActualizada) {
+        return disponibilidadRepository.findById(id).map(disponibilidad -> {
+            disponibilidad.setMedico(disponibilidadActualizada.getMedico());
+            disponibilidad.setDiaSemana(disponibilidadActualizada.getDiaSemana());
+            disponibilidad.setHoraInicio(disponibilidadActualizada.getHoraInicio());
+            disponibilidad.setHoraFin(disponibilidadActualizada.getHoraFin());
+            return disponibilidadRepository.save(disponibilidad);
+        }).orElseThrow(() -> new RuntimeException("Disponibilidad no encontrada con id: " + id));
+    }
+
+    public void eliminar(Long id) {
+        disponibilidadRepository.deleteById(id);
+    }
+}
