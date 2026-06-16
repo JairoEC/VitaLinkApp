4572283a7d09c8459d215ee6b24bcbf69805ceb0
Jennifer <jackcorrea49@gmail.com>
2026-06-16 12:36:47 -0500

    eliminacion entidades Paciente, Medico, Especialidad y Disponibilidad con relaciones JPA en el citas_service

--- vitalinkapp_citas_service/src/main/java/cibertec/edu/pe/repository/DisponibilidadRepository.java
+++
@@ -1,7 +0,0 @@
-package cibertec.edu.pe.repository;
-
-import cibertec.edu.pe.model.Disponibilidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisponibilidadRepository extends JpaRepository<Disponibilidad, Long> {
-}
--- vitalinkapp_citas_service/src/main/java/cibertec/edu/pe/repository/MedicoRepository.java
+++
@@ -1,7 +0,0 @@
-package cibertec.edu.pe.repository;
-
-import cibertec.edu.pe.model.Medico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-}
